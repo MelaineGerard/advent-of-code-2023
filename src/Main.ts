399a7e85aa0d9342210ebfc53fa199069f963786
Melaine Gérard <pro@melaine-gerard.fr>
2023-12-04 15:36:23 +0100

    :sparkles: ajout lecture du fichier d'input

--- src/Main.ts
+++ src/Main.ts
@@ -1,2 +1,17 @@
-import { config } from "dotenv";
+import {config} from "dotenv";
+import { readFileSync } from "fs";
+import { env } from "process";
+
 config()
+
+let fileName = env.INPUT_FILE as string
+
+let fileContent = readFileSync(fileName).toString()
+
+let lines = fileContent.split("\n")
+
+for (let line of lines) {
+    if (line.length === 0) continue
+
+    console.log(line)
+}
