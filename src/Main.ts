627e9f2b2a5aeeb14e412b1f59833f15b4151c36
Melaine Gérard <pro@melaine-gerard.fr>
2023-12-04 11:14:50 +0100

    :sparkles: Jour 1 - 1ère étoile

--- src/Main.ts
+++ src/Main.ts
@@ -1,2 +1,33 @@
-import { config } from "dotenv";
+import {config, parse} from "dotenv";
+import { readFileSync } from "fs";
+import { env } from "process";
+
 config()
+
+let fileName = env.INPUT_FILE as string
+
+let fileContent = readFileSync(fileName).toString()
+
+let lines = fileContent.split("\n")
+
+let total = 0
+
+for (let line of lines) {
+    let firstNumber = -1
+    let lastNumber = -1
+    for (let i = 0; i < line.length; i++) {
+        if (!isNaN(parseInt(line[i]))) {
+            if (firstNumber === -1) {
+                firstNumber = parseInt(line[i])
+            }
+            lastNumber = parseInt(line[i])
+        }
+    }
+
+
+    let newNumber = firstNumber.toString() + lastNumber.toString()
+
+        total += parseInt(newNumber)
+}
+
+console.log(`Le total est : ${total} !`)
